0345395ac55a3f2352a56750931a538f2dbdbf33
varghese1994 <infovarghese94@gmail.com>
2021-01-08 15:36:07 +0530

    final commit

--- client/src/App.js
+++ client/src/App.js
@@ -15,11 +15,9 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile'
-
-
-
-
+import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 
 //redux
 import {Provider} from 'react-redux';
@@ -55,6 +53,9 @@ const App = () => {
               <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
               <PrivateRoute exact path="/add-experience" component={AddExperience}/>
               <PrivateRoute exact path="/add-education" component={AddEducation}/>
+              <PrivateRoute exact path="/posts" component={Posts}/>
+              <PrivateRoute exact path="/posts/:id" component={Post}/>
+
 
             </Switch>
           </section>
---
+++ client/src/actions/post.js
@@ -0,0 +1,162 @@
+import axios from 'axios';
+import {
+    GET_POSTS,
+    POST_ERROR,
+    UPDATE_LIKES,
+    DELETE_POST,
+    ADD_POST,
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
+} from '../actions/types'
+import {setAlert} from './alert'
+
+//Get posts
+export const getPosts = () => async dispatch =>{
+    try {
+        const res = await axios.get('/api/posts');
+        dispatch({
+            type: GET_POSTS,
+            payload:res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload:{ msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+//Get post
+export const getPost = id => async dispatch =>{
+    try {
+        const res = await axios.get(`/api/posts/${id}`);
+        dispatch({
+            type: GET_POST,
+            payload:res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload:{ msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+//add like
+export const addLike = postId => async dispatch =>{
+    try {
+        const res = await axios.put(`/api/posts/like/${postId}`)
+        dispatch({
+            type:UPDATE_LIKES,
+            payload:{ postId,likes: res.data}
+        })
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload:{msg:err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+//remove like
+export const removeLike = postId => async dispatch =>{
+    try {
+        const res = await axios.put(`/api/posts/unlike/${postId}`)
+        dispatch({
+            type:UPDATE_LIKES,
+            payload:{ postId,likes: res.data}
+        })
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload:{msg:err.response.statusText, status: err.response.status}
+        })
+    }
+}
+//delete post
+export const deletePost = postId => async dispatch =>{
+    try {
+        await axios.delete(`/api/posts/${postId}`)
+        dispatch({
+            type:DELETE_POST,
+            payload:postId
+        })
+        dispatch(
+            setAlert('Post Removed', 'success')
+        )
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload:{msg:err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+//add post
+export const addPost = formData => async dispatch =>{
+    try {
+        const config ={
+            headers:{
+                'Content-Type' : 'application/json'
+            }
+        }
+        const res = await axios.post('/api/posts',formData,config)
+        dispatch({
+            type:ADD_POST,
+            payload:res.data
+        })
+        dispatch(
+            setAlert('Post Added', 'success')
+        )
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload:{msg:err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+//add comment
+export const addComment = (postId,formData) => async dispatch =>{
+    try {
+        const config ={
+            headers:{
+                'Content-Type' : 'application/json'
+            }
+        }
+        const res = await axios.post(`/api/posts/comment/${postId}`,formData,config)
+        dispatch({
+            type:ADD_COMMENT,
+            payload:res.data
+        })
+        dispatch(
+            setAlert('Comment added', 'success')
+        )
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload:{msg:err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+//delete comment
+export const deleteComment = (postId,commentId) => async dispatch =>{
+    try {
+        
+        await axios.delete(`/api/posts/comment/${postId}/${commentId}`)
+        dispatch({
+            type:REMOVE_COMMENT,
+            payload:commentId
+        })
+        dispatch(
+            setAlert('Remove comment', 'success')
+        )
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload:{msg:err.response.statusText, status: err.response.status}
+        })
+    }
+}
